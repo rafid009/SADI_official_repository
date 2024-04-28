b7de6363a38f3765e9f05b2c167b383b6d8267b4
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2024-04-28 09:35:49 -0700

    AWN, used dask 1-13

--- datasets/process_awn_v2.py
+++ datasets/process_awn_v2.py
@@ -46,70 +46,71 @@ count_idx = 0
 for file in os.listdir(folder):
     
     if file.endswith('.csv'):
-        try:
-            df = pd.read_csv(f"{folder}/{file}", dtype={
-                'TSTAMP_PST': np.datetime64, 
-                ' UNIT_ID': int, 
-                ' STATION_NAME': str, 
-                ' LATITUDE': np.float32, 
-                ' LONGITUDE': np.float32, 
-                ' ELEVATION_FEET': np.float32, 
-                ' AIR_TEMP_F': np.float32, 
-                ' SECOND_AIR_TEMP_F': np.float32, 
-                'AIR_TEMP_10M_F': np.float32, 
-                ' RELATIVE_HUMIDITY_%': np.float32, 
-                ' DEWPOINT_F': np.float32, 
-                ' LEAF_WETNESS': np.float32, 
-                ' PRECIP_INCHES': np.float32, 
-                ' SECOND_PRECIP_INCHES': np.float32, 
-                ' WIND_DIRECTION_2M_DEG': np.float32, 
-                ' WIND_SPEED_2M_MPH': np.float32, 
-                ' WIND_SPEED_MAX_2M_MPH': np.float32, 
-                ' WIND_DIRECTION_10M_DEG': np.float32, 
-                ' WIND_SPEED_10M_MPH': np.float32, 
-                ' WIND_SPEED_MAX_10M_MPH': np.float32, 
-                ' SOLAR_RAD_WM2': np.float32, 
-                ' SOIL_TEMP_2_IN_DEGREES_F': np.float32, 
-                '  SOIL_TEMP_8_IN_DEGREES_F': np.float32, 
-                '  SOIL_WP_2_IN_KPA': np.float32, 
-                '  SOIL_WP_8_IN_KPA': np.float32, 
-                '  SOIL_MOIS_8_IN_%': np.float32
-            }) 
+
+        df = pd.read_csv(f"{folder}/{file}", dtype={
+            'TSTAMP_PST': np.datetime64, 
+            ' UNIT_ID': int, 
+            ' STATION_NAME': str, 
+            ' LATITUDE': np.float32, 
+            ' LONGITUDE': np.float32, 
+            ' ELEVATION_FEET': np.float32, 
+            ' AIR_TEMP_F': np.float32, 
+            ' SECOND_AIR_TEMP_F': np.float32, 
+            'AIR_TEMP_10M_F': np.float32, 
+            ' RELATIVE_HUMIDITY_%': np.float32, 
+            ' DEWPOINT_F': np.float32, 
+            ' LEAF_WETNESS': np.float32, 
+            ' PRECIP_INCHES': np.float32, 
+            ' SECOND_PRECIP_INCHES': np.float32, 
+            ' WIND_DIRECTION_2M_DEG': np.float32, 
+            ' WIND_SPEED_2M_MPH': np.float32, 
+            ' WIND_SPEED_MAX_2M_MPH': np.float32, 
+            ' WIND_DIRECTION_10M_DEG': np.float32, 
+            ' WIND_SPEED_10M_MPH': np.float32, 
+            ' WIND_SPEED_MAX_10M_MPH': np.float32, 
+            ' SOLAR_RAD_WM2': np.float32, 
+            ' SOIL_TEMP_2_IN_DEGREES_F': np.float32, 
+            '  SOIL_TEMP_8_IN_DEGREES_F': np.float32, 
+            '  SOIL_WP_2_IN_KPA': np.float32, 
+            '  SOIL_WP_8_IN_KPA': np.float32, 
+            '  SOIL_MOIS_8_IN_%': np.float32
+        }) 
+        x = []
+        count = 0
+        for i in range(len(df)):
+            count+=1
+            if count == m:
+                X.append(np.stack(x))
+                x = []
+                count = 0
+            else:
+                x.append(df.iloc[i][features])
+        if len(x) < m:
+            adds = m - len(x)
+            
+            for i in range(len(adds)):
+                y = []
+                for j in range(len(x[0])):
+                    y.append(np.nan)
+                x.append(y)
+            X.append(np.stack(x))
             x = []
             count = 0
-            for i in range(len(df)):
-                count+=1
-                if count == m:
-                    X.append(np.stack(x))
-                    x = []
-                    count = 0
-                else:
-                    x.append(df.iloc[i][features])
-            if len(x) < m:
-                adds = m - len(x)
-                
-                for i in range(len(adds)):
-                    y = []
-                    for j in range(len(x[0])):
-                        y.append(np.nan)
-                    x.append(y)
-            index += 1
-            if index % 60 == 0:
-                X = da.array(X)
-                print(f"X: {X.shape}")
-                
-                if not os.path.isdir(out_folder):
-                    os.makedirs(out_folder)
-                np.save(f"{out_folder}/X_train_{count_idx}.npy", X)
-                count_idx += 1
-                X = []
-                x = []
-        except:
-            continue
-X = da.array(X)
-print(f"X: {X.shape}")
-if not os.path.isdir(out_folder):
-    os.makedirs(out_folder)
-np.save(f"{out_folder}/X_train_{count_idx}.npy", X)
+        index += 1
+        if index % 60 == 0:
+            X = np.array(X)
+            print(f"X: {X.shape}")
+            
+            if not os.path.isdir(out_folder):
+                os.makedirs(out_folder)
+            np.save(f"{out_folder}/X_train_{count_idx}.npy", X)
+            count_idx += 1
+            X = []
+if index % 60 != 0:
+    X = np.array(X)
+    print(f"X: {X.shape}")
+    if not os.path.isdir(out_folder):
+        os.makedirs(out_folder)
+    np.save(f"{out_folder}/X_train_{count_idx}.npy", X)
 
             
